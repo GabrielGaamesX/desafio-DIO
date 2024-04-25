9d657b3b698df579acfb6ab9a652ac4e125570ac
GabrielGaamesX <gabriel.games.work@gmail.com>
2024-04-24 22:41:47 -0300

    Finalizando Contador de elo

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-let variavelTeste ='gabriel'
-console.log(variavelTeste)
-console.log(variavelTeste + 1)
+let xpDoJogador = 1000
+let classificação = ''
+let Nome = 'Gabriel'
+
+
+if(xpDoJogador <= 1000){
+    let classificação = 'Ferro'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 1001 && xpDoJogador <= 2000 ){
+    let classificação = 'Bronze'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 2001 && xpDoJogador <= 5000 ){
+    let classificação = 'Prata'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 5001 && xpDoJogador <= 7000 ){
+    let classificação = 'Ouro'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 7001 && xpDoJogador <= 8000 ){
+    let classificação = 'Platina'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 8001 && xpDoJogador <= 9000 ){
+    let classificação = 'Ascedente'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 9001 && xpDoJogador <= 10000 ){
+    let classificação = 'Imortal'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
+if (xpDoJogador >= 10001 ){
+    let classificação = 'Radiante'
+    console.log("O Herói de nome " +Nome+ " está no nível "+classificação+ ", Atualmente com " +xpDoJogador+ " Pontos")
+}
